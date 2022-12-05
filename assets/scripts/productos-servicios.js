8a00ff40b41ab9c1862031a35af4cc03951cce20
alexis <alexis@alexis>
2022-12-05 02:34:39 -0500

    final3

--- assets/scripts/productos-servicios.js
+++ assets/scripts/productos-servicios.js
@@ -1,11 +1,11 @@
 
 
-const listaProductos = () => fetch("http://api.alexisrodriguez.tk:8080/productos").then(respuesta => respuesta.json());
+const listaProductos = () => fetch("https://api.alexisrodriguez.tk/productos").then(respuesta => respuesta.json());
 
     
 const agregarProducto = (nombre, precio, url, categoria, descripcion) => {
   
-  return fetch("http://api.alexisrodriguez.tk:8080/productos", {
+  return fetch("https://api.alexisrodriguez.tk/productos", {
     method: "POST",
     headers: {
       "Content-Type": "application/json" 
@@ -15,18 +15,18 @@ const agregarProducto = (nombre, precio, url, categoria, descripcion) => {
 };
 
 const eliminarProducto = (id) => {
-  return fetch(`http://api.alexisrodriguez.tk:8080/productos/${id}`, { 
+  return fetch(`https://api.alexisrodriguez.tk/productos/${id}`, { 
     method: "DELETE"
   })
 };
 
 const detalleProducto = (id) => {
-  return fetch(`http://api.alexisrodriguez.tk:8080/productos/${id}`).then((respuesta) => respuesta.json()
+  return fetch(`https://api.alexisrodriguez.tk/productos/${id}`).then((respuesta) => respuesta.json()
   );
 };
 
 const infoUsuario = (nombre) => {
-  return fetch(`http://api.alexisrodriguez.tk:8080/users?usuario=${nombre}`).then((respuesta) => respuesta.json()
+  return fetch(`https://api.alexisrodriguez.tk/users?usuario=${nombre}`).then((respuesta) => respuesta.json()
   );
 }
 
